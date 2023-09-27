9a97e509058fe1c1b4198344bbbe54eb4527572f
saro <romar.sanjuan@betterask.erni>
2023-09-27 09:42:30 +0800

    Sync Files

---
+++ MobileXamarinUiTest/AppManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Xamarin.UITest;
+
+namespace Mobile.Xamarin.UiTest
+{
+    public class AppManager
+    {
+        /* Mobile Application Package ID */
+        private const string AndroidPackageId = "<ADD ANDROID PACKAGE ID HERE>";
+        private const string iOSPackageId = "<ADD IOS PACKAGE ID HERE>";
+
+        static IApp _app;
+        public static IApp App
+        {
+            get
+            {
+                if (_app == null)
+                    throw new NullReferenceException("'AppManager.App' not set. Call 'AppInitializer.StartApp()' before trying to access it.");
+                return _app;
+            }
+        }
+
+        static Platform? platform;
+        public static Platform Platform
+        {
+            get
+            {
+                if (platform == null)
+                    throw new NullReferenceException("'AppManager.Platform' not set.");
+                return platform.Value;
+            }
+
+            set
+            {
+                platform = value;
+            }
+        }
+
+        /* Starting the app based on Platform */
+        public static void StartApp()
+        {
+            if (Platform == Platform.Android)
+            {
+                _app = ConfigureApp
+                    .Android
+                    .InstalledApp(AndroidPackageId)
+                    .EnableLocalScreenshots()
+                    .StartApp();
+            }
+
+            if (Platform == Platform.iOS)
+            {
+                _app = ConfigureApp
+                    .iOS
+                    .InstalledApp(iOSPackageId)
+                .EnableLocalScreenshots()
+                .StartApp();
+            }
+        }
+    }
+}
---
+++ MobileXamarinUiTest/TestFixtures/MultipleTestFixture.cs
@@ -0,0 +1,91 @@
+﻿/* Test Fixture for multiple Setup Fixture
+ * Output:
+ * RootFixtureSetup:OneTimeSetUp
+ * FixtureSetup:OneTimeSetUp
+ * Tests:MultipleTestFixture
+ * Tests:OneTimeSetUp
+ * Tests:SetUp
+ * Tests:TC_00001_Test_Case_Title
+ * Tests:TearDown
+ * Tests:SetUp
+ * Tests:TC_00002_Test_Case_Title
+ * Tests:TearDown
+ * Tests:OneTimeTearDown
+ * FixtureSetup:OneTimeTearDown
+ * RootFixtureSetup:OneTimeTearDown
+ */
+
+/* First Layer */
+using NUnit.Framework;
+using Xamarin.UITest;
+
+[SetUpFixture]
+public class RootFixtureSetup
+{
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+    }
+
+    [OneTimeTearDown]
+    public void OneTimeTearDown()
+    {
+    }
+}
+
+namespace Mobile.Xamarin.UiTest.TestFixtures
+{
+    /* Second Layer */
+    [SetUpFixture]
+    public class FixtureSetup
+    {
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+        }
+    }
+
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+
+    public abstract class MultipleTestFixture
+    {
+        protected IApp _app => AppManager.App;
+
+        /* Platform Constructor */
+        public MultipleTestFixture(Platform platform)
+        {
+            AppManager.Platform = platform;
+        }
+
+        /* Single App Launch */
+        [OneTimeSetUp]
+        public void AppLaunch()
+        {
+            AppManager.StartApp();
+        }
+
+        /* Setup Before Executing the test */
+        [SetUp]
+        public void BeforeEachTest()
+        {
+        }
+
+        /* Setup after Executing the test */
+        [TearDown]
+        public void AfterEachTest()
+        {
+        }
+
+        [OneTimeTearDown]
+        public void Cleanup()
+        {
+
+        }
+    }
+}
